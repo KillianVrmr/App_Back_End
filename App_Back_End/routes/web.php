394066729b1cbfe55a62a10681b8872b881582fd
Lisa-CtrlShiftZ <heesterslisa@gmail.com>
2025-06-04 09:09:45 +0200

    Sidebar and globar colors in css

--- App_Back_End/routes/web.php
+++ App_Back_End/routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+ 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/create_project', function () {
     return view('create_project');
+});
+
+//for testing
+Route::get('/sidebar', function () {
+    return view('components.sidebar');
 });
