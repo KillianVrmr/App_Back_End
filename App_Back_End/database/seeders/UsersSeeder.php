bc0de13457e64e6a2f8b19987f7c4d955ee550c1
BtheBelgian <147116285+BtheBelgian@users.noreply.github.com>
2025-06-12 09:16:59 +0200

    adjust seeders to current database

--- App_Back_End/database/seeders/UsersSeeder.php
+++ App_Back_End/database/seeders/UsersSeeder.php
@@ -15,11 +15,11 @@ class UsersSeeder extends Seeder
     public function run(): void
     {
         User::insert([
-            ['name' => 'Alice', 'lastname' => 'Smith', 'email' => 'alice@example.com', 'password' => Hash::make('password'), 'function_id' => 1, 'role_id' => 1, 'emergency_contact' => '1234567890', 'blood_type' => 'A+'],
-            ['name' => 'Bob', 'lastname' => 'Jones', 'email' => 'bob@example.com', 'password' => Hash::make('password'), 'function_id' => 2, 'role_id' => 2, 'emergency_contact' => '2345678901', 'blood_type' => 'B+'],
-            ['name' => 'Carol', 'lastname' => 'Taylor', 'email' => 'carol@example.com', 'password' => Hash::make('password'), 'function_id' => 3, 'role_id' => 1, 'emergency_contact' => '3456789012', 'blood_type' => 'O-'],
-            ['name' => 'Dave', 'lastname' => 'Brown', 'email' => 'dave@example.com', 'password' => Hash::make('password'), 'function_id' => 1, 'role_id' => 2, 'emergency_contact' => '4567890123', 'blood_type' => 'AB+'],
-            ['name' => 'Eve', 'lastname' => 'White', 'email' => 'eve@example.com', 'password' => Hash::make('password'), 'function_id' => 2, 'role_id' => 1, 'emergency_contact' => '5678901234', 'blood_type' => 'A-'],
+            ['name' => 'Alice', 'lastname' => 'Smith', 'email' => 'alice@example.com', 'password' => Hash::make('password'), 'function_id' => 1, 'role_id' => 1,'emergency_contact' => 'Bob', 'contact_number' => '1234567890', 'blood_type' => 'A+'],
+            ['name' => 'Bob', 'lastname' => 'Jones', 'email' => 'bob@example.com', 'password' => Hash::make('password'), 'function_id' => 2, 'role_id' => 2, 'emergency_contact' => 'Bob', 'contact_number' => '2345678901', 'blood_type' => 'B+'],
+            ['name' => 'Carol', 'lastname' => 'Taylor', 'email' => 'carol@example.com', 'password' => Hash::make('password'), 'function_id' => 3, 'role_id' => 1, 'emergency_contact' => 'Bob', 'contact_number' => '3456789012', 'blood_type' => 'O-'],
+            ['name' => 'Dave', 'lastname' => 'Brown', 'email' => 'dave@example.com', 'password' => Hash::make('password'), 'function_id' => 1, 'role_id' => 2, 'emergency_contact' => 'Bob', 'contact_number' => '4567890123', 'blood_type' => 'AB+'],
+            ['name' => 'Eve', 'lastname' => 'White', 'email' => 'eve@example.com', 'password' => Hash::make('password'), 'function_id' => 2, 'role_id' => 1, 'emergency_contact' => 'Bob', 'contact_number' => '5678901234', 'blood_type' => 'A-'],
         ]);
     }
 }
