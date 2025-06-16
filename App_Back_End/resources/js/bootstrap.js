f64cd6e1de3a1f414c0cd73fdd295dc202a682d4
BtheBelgian <147116285+BtheBelgian@users.noreply.github.com>
2025-06-16 09:51:19 +0200

    update composer

--- App_Back_End/resources/js/bootstrap.js
+++ App_Back_End/resources/js/bootstrap.js
@@ -14,7 +14,7 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
+    broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: import.meta.env.VITE_REVERB_PORT,
